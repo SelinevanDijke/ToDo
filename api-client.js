d085ec063b64c26dc5e3d91d3dca8e470bcbcbf6
Seline van Dijke <seline.vandijke98@gmail.com>
2022-02-01 20:30:16 +0100

    Eerste deel opdracht

---
+++ api-client.js
@@ -0,0 +1,28 @@
+// // // Make GET request
+// const LoadData = async () => {
+//   const res = await fetch("http://localhost:3000/", {
+//         method: 'GET',
+//         headers: {'Content-Type': 'application/json'}
+//   });
+// }
+
+// const baseUrl = "http://localhost:3000/";
+// const data = { description: "Koop melk", done: false };
+//  fetch(baseUrl, {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(data),
+// })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log('Success:', data);
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//     });
+
+
+
+// Het probleem is nu dat het wel wordt gepost, maar de pagina blijft constant laden waardoor deze date heel veel wordt gepost.
