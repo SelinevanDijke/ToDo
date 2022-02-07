a99e71ccb9c2a82d07f09ee3a69adfe99c93da04
Seline van Dijke <seline.vandijke98@gmail.com>
2022-02-07 16:31:13 +0100

    Code geupdate

--- api-client.js
+++ api-client.js
@@ -1,28 +1,24 @@
-// // // Make GET request
-// const LoadData = async () => {
-//   const res = await fetch("http://localhost:3000/", {
-//         method: 'GET',
-//         headers: {'Content-Type': 'application/json'}
-//   });
-// }
+// Variabelen
+const baseUrl = "HTTP://localhost:3000/";
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
-// const baseUrl = "http://localhost:3000/";
 // const data = { description: "Koop melk", done: false };
-//  fetch(baseUrl, {
+// fetch(baseUrl, {
 //     method: "POST",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-// })
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log('Success:', data);
-//     })
-//     .catch((error) => {
-//         console.error('Error:', error);
-//     });
+//     headers: myHeaders,
+//     body: JSON.stringify(data)
+// });
+// console.log(data)
+
+const LoadData = async () => {
+  const res = await fetch("http://localhost:3000/", {
+        method: 'GET',
+        headers: myHeaders
+  });
+    console.log(res);
+}
+LoadData();
 
 
 
-// Het probleem is nu dat het wel wordt gepost, maar de pagina blijft constant laden waardoor deze date heel veel wordt gepost.
---
+++ script.js
@@ -0,0 +1,14 @@
+// Variabelen
+const createTask = document.getElementById("add-text");
+const addButton = document.getElementById("add-button"); 
+const getList = document.getElementById("todo-list");
+
+// Functie voor de post
+addButton.addEventListener("click", () => {
+    // const toDoItem = _.description;
+    
+});
+
+
+
+
