bf73bfa653153a541e4df4b850d90079b766a007
Seline van Dijke <seline.vandijke98@gmail.com>
2022-02-16 19:57:25 +0100

    Post functie schrijven

--- api-client.js
+++ api-client.js
@@ -3,6 +3,9 @@ const baseUrl = "HTTP://localhost:3000/";
 let myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
+const createTask = document.getElementById("add-text");
+const taskForm = document.getElementById("add-task"); 
+
 
 // GET functie
 const loadData = async () => {
@@ -12,10 +15,48 @@ const loadData = async () => {
   });
   const data = await res.json();
   console.log(data);
+
+  // Voeg de data toe aan de DOM
+  const toDoList = document.getElementById("todo-list");
+  const image = document.createElement('img');
+  image.src = "icon.jpeg";
+
+  let output = '';
+  data.forEach((task) => {
+    output += `<div id="task-item">
+              <li>${task.description}</li> 
+                <img src="bin.png">              
+              </div>`;
+  });
+  toDoList.innerHTML = output;
 }
 loadData();
 
-// POST functie
-// const postData = async () => {
+// Laad de tekst uit de form
+const postData = async () => {
+   fetch(baseUrl, {
+      method: "POST",
+      body: JSON.stringify(),
+      headers: myHeaders
+    })
+}
 
-// }
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(createTask.value);
+  postData();
+});
+
+
+// POST functie
+
+
+
+// const data = {description: "Koop kwark", done: false};
+// fetch(baseUrl, {
+//   method: "POST",
+//   body: JSON.stringify(data),
+//   headers: {
+//       "Content-Type": "application/json",
+//   },
+// });
